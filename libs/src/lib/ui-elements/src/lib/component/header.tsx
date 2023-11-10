c209d6ffd6c22876bd4abffb9e6db6fdade4bfff
SnehaSharma7 <sharmasneha2546@gmail.com>
2023-11-10 15:38:27 +0530

    fix:first commit(header,side-nav and home page)

--- libs/src/lib/ui-elements/src/lib/component/header.tsx
+++ libs/src/lib/ui-elements/src/lib/component/header.tsx
@@ -1,4 +1,33 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export async function Header() {
-    return <h1>Header File</h1>;
-  }
-  
+  return (
+    <div className="nav-container">
+      <div className="nav-logo">
+        <Link href="/">
+          <Image
+            src="../imgs/settlemint-logo.svg"
+            width={200}
+            height={40}
+            alt=""
+          />
+        </Link>
+      </div>
+      <div className="right-nav">
+        <div className="nav-wallet">
+          <button>
+            <img src="../imgs/Group.svg" alt="" />
+            <p>Connect Metamask Wallet</p>
+          </button>
+        </div>
+        <div className="logout">
+          <button>
+            <img src="../imgs/logout.svg" alt="" />
+            <p>LOGOUT</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- libs/src/lib/ui-elements/src/lib/component/leftNav.tsx
+++ libs/src/lib/ui-elements/src/lib/component/leftNav.tsx
@@ -1,10 +1,27 @@
-export async function LeftNav() {
-    return (
-      <div className="row">
-          <div className="col-md-6">sdfsf</div>
-          <div className="col-md-6">sdfsf</div>
-        </div>
+import Link from 'next/link';
 
-)
-  }
-  
+export async function LeftNav() {
+  return (
+    <nav className="navbar">
+      <div className="left-side">
+        <ul className="dashboard-list">
+          <li className="create ">
+            <Link href="/" className="active">
+              <img src="../imgs/add.svg" alt="" />
+              <p>Create Stable Coin</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">Operate Stable Coins</Link>
+          </li>
+          <li>
+            <Link href="/">Role Management</Link>
+          </li>
+          <li>
+            <Link href="/">Stable Coin Details</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
