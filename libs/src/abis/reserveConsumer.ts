4546a0b430e09c89637fe59dd5132ae01ff2f24b
Ashlesh Sortee <ashlesh@settlemint.com>
2023-11-17 16:27:58 +0530

    Add abis

---
+++ libs/src/abis/reserveConsumer.ts
@@ -0,0 +1,39 @@
+export const reserveConsumer = [
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "feedAddress",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "getDecimals",
+    "outputs": [
+      {
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getLatestReserve",
+    "outputs": [
+      {
+        "internalType": "int256",
+        "name": "",
+        "type": "int256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
---
+++ libs/src/abis/stablecoinFactory.ts
@@ -0,0 +1,218 @@
+export const stablecoiFactoryAbi = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'initialOwner',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: 'address',
+        name: 'reserveConsumerAddress',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'address',
+        name: 'tokenAddress',
+        type: 'address',
+      },
+      {
+        indexed: false,
+        internalType: 'bool',
+        name: 'capped',
+        type: 'bool',
+      },
+    ],
+    name: 'NewStablecoin',
+    type: 'event',
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'previousOwner',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'OwnershipTransferred',
+    type: 'event',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bool',
+        name: 'isOffChainOracle',
+        type: 'bool',
+      },
+      {
+        internalType: 'address',
+        name: 'feedAddress',
+        type: 'address',
+      },
+      {
+        internalType: 'string',
+        name: 'name',
+        type: 'string',
+      },
+      {
+        internalType: 'string',
+        name: 'symbol',
+        type: 'string',
+      },
+      {
+        internalType: 'uint256',
+        name: 'initialSupply',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'maxSupply',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'defaultAdmin',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'pauser',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'minter',
+        type: 'address',
+      },
+    ],
+    name: 'deployStablecoin',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'getAllStableCoins',
+    outputs: [
+      {
+        internalType: 'address[]',
+        name: '',
+        type: 'address[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'stableCoinAddress',
+        type: 'address',
+      },
+    ],
+    name: 'getStablecoinDetails',
+    outputs: [
+      {
+        components: [
+          {
+            internalType: 'string',
+            name: 'name',
+            type: 'string',
+          },
+          {
+            internalType: 'string',
+            name: 'symbol',
+            type: 'string',
+          },
+          {
+            internalType: 'uint256',
+            name: 'initialSupply',
+            type: 'uint256',
+          },
+          {
+            internalType: 'uint256',
+            name: 'maxSupply',
+            type: 'uint256',
+          },
+          {
+            internalType: 'address',
+            name: 'defaultAdmin',
+            type: 'address',
+          },
+          {
+            internalType: 'address',
+            name: 'pauser',
+            type: 'address',
+          },
+          {
+            internalType: 'address',
+            name: 'minter',
+            type: 'address',
+          },
+        ],
+        internalType: 'struct StableCoinFactory.Stablecoin',
+        name: '',
+        type: 'tuple',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'renounceOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'newOwner',
+        type: 'address',
+      },
+    ],
+    name: 'transferOwnership',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
